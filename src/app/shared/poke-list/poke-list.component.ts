3d16b310feeba9e53d675624881b7ae03a9f3601
unknown <alexandre8603@gmail.com>
2023-02-02 09:53:16 -0300

    create pokeapi service

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PokeapiService } from '../../service/pokeapi.service';
 
 @Component({
   selector: 'poke-list',
   templateUrl: './poke-list.component.html',
   styleUrls: ['./poke-list.component.scss']
 })
-export class PokeListComponent {
+export class PokeListComponent implements OnInit {
+
+  constructor(
+    private pokeapiService : PokeapiService
+  ) {}
+
+  ngOnInit(): void {
+    this.pokeapiService.apiListAllPokemons.subscribe(
+      res => res
+    );
+    
+  }
 
 }
